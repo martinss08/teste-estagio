2cf9204d7c3d3044518d198cc11ccf6b80355c15
Joao Victor <victorcostamartins0@gmail.com>
2025-11-06 01:15:28 -0300

    Criação do Controller e Models

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -2,9 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tarefa;
 use Illuminate\Http\Request;
+use App\Http\Requests\TarefaRequest;
+use Illuminate\Database\Eloquent\Model;
 
 class TarefaController extends Controller
 {
-    //
+    protected $model;
+
+    public function __construct(Tarefa $model)
+    {
+        $this->model = $model;
+    }
+
+   public function index() 
+    {
+        $tarefas = $this->model->all();
+        return view('welcome', ['tarefas' => $tarefas]);
+    }
+
+    public function create()
+    {
+        return view('tarefa/tarefa-form');
+    }
+
+    public function store(TarefaRequest $request)
+    {
+        $dados = $request->validated();
+
+        $this->model->create($dados);
+
+        return redirect('/')->with(['success' => 'Tarefa cadastrado com sucesso!']);
+    }
+
+    public function edit($id)
+    {
+        $tarefaEdit = $this->model->find($id);
+
+        return view('tarefa/tarefa-form', ['tarefaEdit' => $tarefaEdit]);
+
+    }
+
+    public function update($id, TarefaRequest $request)
+    {
+        $tarefa = $this->model->find($id);
+
+        $dados = $request->validated();
+
+        $tarefa->update($dados);
+    
+        return redirect('/')->with(['success' => 'Tarefa editada com sucesso!']);
+    }
+
+    public function destroy($id) 
+    {
+        $tarefa = $this->model->find($id);
+
+        $tarefa->delete();
+
+        return redirect('/')->with(['success' => 'Tarefa deletada com sucesso!']);
+    }
 }
--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tarefa extends Model
 {
-    //
+    protected $fillable = [
+        'titulo',
+        'descricao',
+        // 'status'
+    ];
 }
