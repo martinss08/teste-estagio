112bb3fff0ae187c1244218ad8958cb4f3bf2638
Joao Victor <victorcostamartins0@gmail.com>
2025-11-06 01:14:39 -0300

    validação Request

---
+++ app/Http/Requests/TarefaRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TarefaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'titulo' => 'required|min:3|max:150',
+            'descricao' => 'max:255',
+            // 'status' => 'required'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'titulo.required' => 'O campo título é obrigatório.',
+            'titulo.min'      => 'O título deve ter no mínimo 3 caracteres.',
+            'titulo.max'      => 'O título não pode ter mais que 255 caracteres.',
+            // 'titulo.unique' => 'Este usuário já possui uma tarefa com esse título.',
+
+            'descricao.max'   => 'A descrição não pode ter mais que 250 caracteres.',
+        
+            'status.required' => 'Por favor, selecione o satus'
+        ];
+    }
+}
