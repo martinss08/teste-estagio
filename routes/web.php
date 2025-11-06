6c15f646d5f59665e314a9bb7ff3fe5d3d7a4b73
Joao Victor <victorcostamartins0@gmail.com>
2025-11-06 01:11:40 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\TarefaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TarefaController::class, 'index'])->name('tarefa.index');
+Route::get('/tarefa/create', [TarefaController::class, 'create']);
+Route::post('/tarefa/store', [TarefaController::class, 'store'])->name('tarefa.store');
+Route::get('/tarefa/edit/{id}', [TarefaController::class, 'edit'])->name('tarefa.edit');
+Route::put('/tarefa/update/{id}', [TarefaController::class, 'update'])->name('tarefa.update');
+Route::delete('/tarefa/delete/{id}', [TarefaController::class, 'destroy'])->name('tarefa.delete');
