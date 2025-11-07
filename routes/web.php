ba1bbc272e1f8c7811492fb7be8fa26e18e323ca
Joao Victor <victorcostamartins0@gmail.com>
2025-11-07 11:20:00 -0300

    ajuste routs

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\TarefaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TarefaController::class, 'index'])->name('tarefa.index');
-Route::get('/tarefa/create', [TarefaController::class, 'create']);
-Route::post('/tarefa/store', [TarefaController::class, 'store'])->name('tarefa.store');
-Route::get('/tarefa/edit/{id}', [TarefaController::class, 'edit'])->name('tarefa.edit');
-Route::put('/tarefa/update/{id}', [TarefaController::class, 'update'])->name('tarefa.update');
-Route::delete('/tarefa/delete/{id}', [TarefaController::class, 'destroy'])->name('tarefa.delete');
+Route::get('/', function () {
+    return redirect()->route('tarefas.index');
+});
+
+Route::resource('tarefas', TarefaController::class);
